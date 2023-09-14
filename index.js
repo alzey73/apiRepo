96670c2a19d1adfb1279f0665c13b3ecaa9f0283
alzey73 <seozoglu@gmail.com>
2023-09-14 23:25:53 +0300

    09-14

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-ss
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+
+const products = [
+    { id: 1, name: "iphone 12", price: 20000 },
+    { id: 2, name: "iphone 13", price: 30000 },
+    { id: 1, name: "iphone 14", price: 40000 }
+];
+
+app.get("/", (req, res) => {
+    res.send(products[0]);
+});
+
+app.get("/api/products", (req, res) => {
+    res.send(products);
+});
+
+app.post("/api/products", (req, res) => {
+    const product = {
+        id: products.length + 1,
+        name: req.body.name,
+        price: req.body.price
+    };
+
+    products.push(product);
+    res.send(product);
+
+});
+
+app.get("/api/products/:id", (req, res) => {
+
+    const product = products.find(p => p.id == req.params.id);
+
+    if (!product) {
+        res.status(404).send("aradığınız ürün bulunamadı");
+    }
+
+    res.send(product);
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
